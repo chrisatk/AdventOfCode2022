27d16329ad835bd0760323d1f347cf3cdcdfb73d
Chris Atkinson <46910324+chrisatk@users.noreply.github.com>
2022-12-06 13:27:39 +0000

    day 6

---
+++ 6a.py
@@ -0,0 +1,15 @@
+with open('6input.txt','r') as f:
+  for ln in f:
+    ln = ln.strip()
+
+    for i in range(4,len(ln)):
+      block = ln[i-4:i]
+      match = 0
+      for c in block:
+        if (block.count(c) > 1):
+          match = 1
+      if (match == 0):
+        print(block + " after "+ str(i))
+        break
+  
+# Done reading file
